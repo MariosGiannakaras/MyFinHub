5c3fa45b87371d77efae13c41bbc4cfc8f0308f4
mariog <28479056+MariosGiannakaras@users.noreply.github.com>
2026-08-17 11:44:42 +0300

    security: minimize public health endpoint

--- api/health.ts
+++ api/health.ts
@@ -1,12 +1,8 @@
-import { DATA_SOURCE } from '../server/storage.js';
+import { handleApi, methodNotAllowed, sendJson } from '../server/http.js';
 
 export default async function handler(req: any, res: any) {
-  if (req.method !== 'GET') {
-    res.statusCode = 405;
-    res.setHeader('allow', 'GET');
-    return res.end();
-  }
-  res.setHeader('content-type', 'application/json; charset=utf-8');
-  res.setHeader('cache-control', 'no-store');
-  res.end(JSON.stringify({ ok: true, app: 'RheomIQ', persistence: DATA_SOURCE }));
+  await handleApi(res, async () => {
+    if (req.method !== 'GET') return methodNotAllowed(res, ['GET']);
+    return sendJson(res, 200, { ok: true, app: 'RheomIQ' });
+  });
 }
