e887474e6a8461e15aa01b96e32902a1f0094d5f
mariog <28479056+MariosGiannakaras@users.noreply.github.com>
2026-08-19 13:51:51 +0300

    feat(desktop): add isolated MyFinHub bridge

---
+++ desktop/preload.cjs
@@ -0,0 +1,19 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+const updateChannel = 'myfinhub:update-state';
+
+contextBridge.exposeInMainWorld('myFinHubDesktop', Object.freeze({
+  getInfo: () => ipcRenderer.invoke('myfinhub:get-info'),
+  getUpdateState: () => ipcRenderer.invoke('myfinhub:get-update-state'),
+  checkForUpdates: () => ipcRenderer.invoke('myfinhub:check-updates'),
+  downloadUpdate: () => ipcRenderer.invoke('myfinhub:download-update'),
+  installUpdate: () => ipcRenderer.invoke('myfinhub:install-update'),
+  onUpdateState: (listener) => {
+    if (typeof listener !== 'function') return () => {};
+    const handler = (_event, state) => listener(state);
+    ipcRenderer.on(updateChannel, handler);
+    return () => ipcRenderer.removeListener(updateChannel, handler);
+  },
+  getSetupState: () => ipcRenderer.invoke('myfinhub:get-setup-state'),
+  saveSetup: (value) => ipcRenderer.invoke('myfinhub:save-setup', value),
+}));
